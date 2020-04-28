be57b5de1efc679e5ada198050fce5f11e2732b7
David Meister <thedavidmeister@gmail.com>
2020-04-28 18:47:06 +0400

    wip on plugins

--- crates/holochain/src/core/ribosome/plugin.rs
+++ crates/holochain/src/core/ribosome/plugin.rs
@@ -3,14 +3,30 @@ use super::WasmRibosome;
 use holochain_zome_types::PluginInput;
 use holochain_zome_types::PluginOutput;
 use std::sync::Arc;
-use std::convert::TryInto;
+use std::path::Path;
+use sharedlib::LibArc;
+use sharedlib::FuncArc;
+use holochain_serialized_bytes::prelude::*;
+use sharedlib::Symbol;
+use holo_hash::holo_hash_core::EntryHash;
 
 pub async fn plugin(
     _ribosome: Arc<WasmRibosome>,
     _host_context: Arc<HostContext>,
     _input: PluginInput,
 ) -> PluginOutput {
-    println!("fooo");
+    let target_dir = std::env::var_os("CARGO_TARGET_DIR").unwrap();
+    println!("fooo {:?}", std::process::Command::new("pwd").output().unwrap());
+    let lib_path = Path::new(&target_dir).join("release/libtest_plugin_hash_dumper.so");
+    let hash_dump = unsafe {
+        let lib = LibArc::new(lib_path).unwrap();
+        let hash_dump_symbol: FuncArc<extern "C" fn(SerializedBytes) -> SerializedBytes> = lib.find_func("hash_dump").unwrap();
+        hash_dump_symbol.get()
+    };
+
+    let entry_hash = EntryHash::new(vec![0xdb; 36]);
+    let entry_hash_sb = SerializedBytes::try_from(entry_hash).unwrap();
+    hash_dump(entry_hash_sb);
     PluginOutput::new(().try_into().unwrap())
 }
 
--- crates/test_utils/plugin/hash_dumper/src/lib.rs
+++ crates/test_utils/plugin/hash_dumper/src/lib.rs
@@ -1,6 +1,11 @@
+use holochain_serialized_bytes::prelude::*;
+use holo_hash_core::EntryHash;
+
 #[no_mangle]
-pub extern fn hash_dump(sb: SerializedBytes) -> SerializedBytes {
+pub extern fn hash_dump(ptr: usize, len: usize) -> (usize, usize) {
     let hash: EntryHash = sb.try_into().unwrap();
     println!("{:?}", &hash);
-    hash.try_into().unwrap()
+    let sb: SerializedBytes = hash.try_into().unwrap();
+    let bytes = UnsafeBytes::from(sb);
+
 }
