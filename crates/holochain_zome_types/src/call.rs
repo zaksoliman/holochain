889f032266c120c9c6db942778861ca0781c3528
thedavidmeister <thedavidmeister@gmail.com>
2021-11-10 03:13:16 +0400

    wip on merged call interface

--- crates/holochain_zome_types/src/call.rs
+++ crates/holochain_zome_types/src/call.rs
@@ -1,36 +1,26 @@
 use crate::capability::CapSecret;
-use crate::cell::CellId;
 use crate::zome::FunctionName;
 use crate::zome::ZomeName;
 use crate::ExternIO;
 use holo_hash::AgentPubKey;
 
 #[derive(Clone, Debug, PartialEq, serde::Serialize, serde::Deserialize)]
-pub struct Call {
-    pub to_cell: Option<CellId>,
-    pub zome_name: ZomeName,
-    pub fn_name: FunctionName,
-    pub cap_secret: Option<CapSecret>,
-    pub payload: ExternIO,
-    pub provenance: AgentPubKey,
+pub enum CallRoute {
+    /// Same DNA, same agent.
+    Local,
+    /// Same DNA, different agent.
+    NetworkAgent(AgentPubKey),
+    /// Same agent, different DNA. Specified Hash.
+    DnaHash(DnaHash),
+    /// Same agent, different DNA. Configurable role.
+    DnaRole(DnaRole),
 }
 
-impl Call {
-    pub fn new(
-        to_cell: Option<CellId>,
-        zome_name: ZomeName,
-        fn_name: FunctionName,
-        cap_secret: Option<CapSecret>,
-        payload: ExternIO,
-        provenance: AgentPubKey,
-    ) -> Self {
-        Self {
-            to_cell,
-            zome_name,
-            fn_name,
-            cap_secret,
-            payload,
-            provenance,
-        }
-    }
+#[derive(Clone, Debug, PartialEq, serde::Serialize, serde::Deserialize, Constructor)]
+pub struct Call {
+    pub route: CallRoute,
+    pub zome_role: ZomeRole,
+    pub fn_role: FunctionRole,
+    pub cap_secret: Option<CapSecret>,
+    pub payload: ExternIO,
 }
